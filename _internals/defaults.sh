052481ad50c6d580fc9f6ea6ba8133131285351f
Kevin Allen <kevjallen16@gmail.com>
2022-02-10 17:59:14 -0500

    update for style consistency

--- _internals/defaults.sh
+++ _internals/defaults.sh
@@ -8,6 +8,6 @@ if [[ -z "$AWS_STACK_PATH" ]]; then
   AWS_STACK_PATH='_internals/aws-stack'
 fi
 
-if [[ -z "${GITHUB_SERVER_URL}" ]]; then
+if [[ -z "$GITHUB_SERVER_URL" ]]; then
   GITHUB_SERVER_URL='https://github.com'
 fi
