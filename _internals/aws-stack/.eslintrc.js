533457a51810b307c76843ea7ee203155a884beb
kevjallen <kevjallen16@gmail.com>
2022-02-13 17:01:38 -0500

    feat: aws stack linting and versioning (#9)

---
+++ _internals/aws-stack/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  ignorePatterns: [
+    './node_modules',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'cdk',
+  ],
+  rules: {
+    'no-new': 'off',
+    'import/extensions': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
+};
