9cf73d9bbd49d15ed81410fef2dc2deef91a474d
Kevin Allen <kevjallen16@gmail.com>
2022-02-19 03:00:13 -0500

    refactor pipeline

---
+++ _internals/aws-stack/bin/static-site-pipeline.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { PipelineStack } from '../lib/pipeline-stack';
+import { StaticSiteAppStage } from '../lib/static-site-app-stage';
+
+const app = new cdk.App();
+const cdkAppPath = process.cwd();
+
+const sourceConnectionId = app.node.tryGetContext('sourceConnectionId');
+
+const account = process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT;
+const region = process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION;
+
+new PipelineStack(app, 'StaticSitePipeline', {
+  sourceConnectionArn:
+    `arn:aws:codestar-connections:${region}:${account}:connection/${sourceConnectionId}`,
+  sourceRepo: 'kevjallen/static-site',
+  synthCommands: [
+    'bundle install',
+    'bundle exec jekyll build',
+    `cd ${cdkAppPath}`,
+    'npm install',
+    'npm run cdk synth',
+  ],
+  appStages: [
+    new StaticSiteAppStage(app, 'StaticSite-Preview-000', {
+      siteContentsPath: '../../_site',
+    }),
+  ],
+  buildImage: 'ubuntu-build:v1.0.0',
+  installCommands: [
+    'source $ASDF_SCRIPT && asdf install',
+  ],
+  pipelineName: 'static-site',
+  synthCommandShell: 'bash',
+  sourceBranch: 'cdk-pipelines',
+  synthEnv: {
+    ASDF_SCRIPT: '/root/.asdf/asdf.sh'
+  },
+  synthOutputDir: `${cdkAppPath}/cdk.out`,
+});
--- _internals/aws-stack/lib/pipeline-stack.ts
+++ _internals/aws-stack/lib/pipeline-stack.ts
@@ -1,56 +1,62 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Stage } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, CodeBuildStep } from 'aws-cdk-lib/pipelines';
-import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
+import { BuildSpec, IBuildImage, LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 
 export interface PipelineStackProps extends StackProps {
   sourceConnectionArn: string
   sourceRepo: string
   synthCommands: string[]
 
+  appStages?: Stage[]
+  buildImage?: string
+  installCommands?: string[]
   pipelineName?: string
   sourceBranch?: string
-  synthAptDeps?: string[]
+  synthCommandShell?: string
+  synthEnv?: Record<string, string>
   synthOutputDir?: string
 }
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
-    
+
     const sourceBranch = props.sourceBranch || 'master';
 
-    const systemSetupCommands = !props.synthAptDeps ? [] : [
-      `apt-get update && apt-get install -y ${props.synthAptDeps.join(' ')}`
-    ];
-    const toolSetupCommands = [
-      'apt-get update && apt-get install -y git curl',
-      'git clone $ASDF_REPO $HOME/.asdf --branch $ASDF_VERSION',
-      'source $HOME/.asdf/asdf.sh && asdf install'
-    ];
+    let buildImage: IBuildImage;
+    if (props.buildImage) {
+      const [imageName, imageTag] = props.buildImage.split(':');
+      buildImage = LinuxBuildImage.fromEcrRepository(
+        Repository.fromRepositoryName(this, 'BuildImageRepository', imageName),
+        imageTag,
+      );
+    } else {
+      buildImage = LinuxBuildImage.STANDARD_5_0;
+    }
 
-    const pipeline = new CodePipeline(this, 'SitePipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: props.pipelineName,
-      synth: new CodeBuildStep('Synth', {
-        input: CodePipelineSource.connection(props.sourceRepo, sourceBranch, {
-          connectionArn: props.sourceConnectionArn
-        }),
-        projectName: 
-          props.pipelineName ? `${props.pipelineName}-synth` : undefined,
-        env: {
-          ASDF_REPO: 'https://github.com/asdf-vm/asdf.git',
-          ASDF_VERSION: 'v0.9.0',
-        },
+      synth: new CodeBuildStep('Synthesize', {
         buildEnvironment: {
-          buildImage: LinuxBuildImage.STANDARD_5_0
+          buildImage,
         },
-        installCommands: [
-          ...systemSetupCommands,
-          ...toolSetupCommands
-        ],
         commands: props.synthCommands,
-        primaryOutputDirectory: props.synthOutputDir
-      })
+        env: props.synthEnv,
+        input: CodePipelineSource.connection(props.sourceRepo, sourceBranch, {
+          connectionArn: props.sourceConnectionArn,
+        }),
+        installCommands: props.installCommands,
+        partialBuildSpec: BuildSpec.fromObject({
+          env: {
+            shell: props.synthCommandShell,
+          },
+        }),
+        primaryOutputDirectory: props.synthOutputDir,
+        projectName: props.pipelineName ? `${props.pipelineName}-synth` : undefined,
+      }),
     });
+    props.appStages?.forEach((stage) => { pipeline.addStage(stage); });
   }
 }
